2ecbcf6baa179e764cc831eb79a999ac6f2d51e8
miss-f-g <fatemeghafari98@gmail.com>
2022-07-15 14:00:11 +0430

    3.write drumkit class

---
+++ app.js
@@ -0,0 +1,27 @@
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.index = 0;
+    this.bpm = 150;
+  }
+
+  repeat() {
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    this.index++;
+  }
+
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+}
+
+const drumKit = new DrumKit();
+drumKit.start();
